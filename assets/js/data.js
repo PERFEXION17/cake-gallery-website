3cd72b6c252a5cc1a13d5afbde54d527f998f49a
PERFEXION <chubuike17@gmail.com>
2026-04-21 16:49:17 +0100

    feat: first version of the website

---
+++ assets/js/data.js
@@ -0,0 +1,50 @@
+export const allProducts = [
+  {
+    slug: "daily-cakes",
+    category: "daily-cakes",
+    title: "DAILY CAKES",
+    imgs: [
+      "./assets/imgs/cakes/daily-cakes/daily-cake-1.webp",
+      "./assets/imgs/cakes/daily-cakes/daily-cake-2.webp",
+      "./assets/imgs/cakes/daily-cakes/daily-cake-3.webp",
+      "./assets/imgs/cakes/daily-cakes/daily-cake-4.webp",
+    ],
+    name: '',
+  },
+  {
+    slug: "ceremonial-cakes",
+    category: "ceremonial-cakes",
+    title: "CEREMONIAL CAKES",
+    imgs: [
+      "./assets/imgs/cakes/ceremonial-cakes/ceremonial-cake-1.webp",
+      "./assets/imgs/cakes/ceremonial-cakes/ceremonial-cake-2.webp",
+      "./assets/imgs/cakes/ceremonial-cakes/ceremonial-cake-3.webp",
+      "./assets/imgs/cakes/ceremonial-cakes/ceremonial-cake-4.webp",
+    ],
+    name: '',
+  },
+  {
+    slug: "cookies",
+    category: "cookies",
+    title: "COOKIES",
+    imgs: [
+      "./assets/imgs/cookies/cookies-1.webp",
+      "./assets/imgs/cookies/cookies-2.webp",
+      "./assets/imgs/cookies/cookies-3.webp",
+      "./assets/imgs/cookies/cookies-4.webp",
+    ],
+    name: '',
+  },
+  {
+    slug: "desserts",
+    category: "desserts",
+    title: "DESSERTS",
+    imgs: [
+      "./assets/imgs/desserts/desserts-1.webp",
+      "./assets/imgs/desserts/desserts-2.webp",
+      "./assets/imgs/desserts/desserts-3.webp",
+      "./assets/imgs/desserts/desserts-4.webp",
+    ],
+    name: '',
+  },
+];
